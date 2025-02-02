b8f95a8dd863a0370db0b55e6b3e7cd4f9d14651
Noah <40387956+noahostle@users.noreply.github.com>
2025-02-02 15:32:29 +1100

    Add files via upload

--- diacriticsdecoder.py
+++ diacriticsdecoder.py
@@ -4,9 +4,9 @@ inter=''
 
 for x in list(inp):
 	if ord(x)>256:
-		if x=='\u0332':
+		if x=='\u0332' or x=="\u0329":
 			inter+='1'
-		elif x=='\u0331':
+		elif x=='\u0330' or x=="\u0325":
 			inter+='0'
 		else:
 			inter+=' '
--- diacriticsencoder.py
+++ diacriticsencoder.py
@@ -1,30 +1,49 @@
+import math
+ye = input("Corny mode (y) or regular mode (n) >> ")
+seven = input("Seven bit ascii mode (y/n) >> ")
+shrtrascii=("y" in seven)
+
+if "y" in ye:
+	one='\u0329'
+	zero='\u0325'
+else:
+	one='\u0332'
+	zero='\u0330'
+
+separator='\u0323'
 
 inp = input('enter input string >> ')
 
-sli = input("enter index(es) of character to diacritic (csv) >> ")
+sli = input("enter index(es) of character to diacritic (csv), or -1 for all >> ")
 
 slis = None
 
 if (',' in sli):
 	slis = [int(i)-1 for i in sli.split(',')]
+elif sli=='-1':
+	slis = [j for j, c in enumerate(inp) if not c.isspace()]
 else:
 	sli=int(sli)-1
 
 
 pln = input("enter message to encode in diacritics >> ")
 
-enc = ' '.join(format(ord(c), '08b') for c in pln)
+bytez = '07b' if shrtrascii else '08b'
+enc = ' '.join(format(ord(c), bytez) for c in pln)
 
 
 cph=''
 for x in enc:
 	if x=='1':
-		cph+='\u0332'
+		cph+=one
 	elif x=='0':
-		cph+='\u0331'
+		cph+=zero
 	else:
-		cph+='\u0323'
+		cph+=separator
 
+
+#cph+=separator
+#optional ^
 
 if slis!=None:
 	slis.sort()
@@ -32,19 +51,28 @@ if slis!=None:
 	inp=list(inp)
 	print(str(len(pln))+" "+str(len(slis)))
 
-	perletter=len(pln)//len(slis)
-	remainder = len(pln)%len(slis)
+	ln = len(pln*(8 if shrtrascii else 9))
+	quot=ln//len(slis)
+	remainder=ln%len(slis)
+
+	print(f"{quot} {remainder}")
+
 
 	for y in range(0, len(slis)):
-		if remainder > 0:
-			n = (perletter+1)*9
+		if inp[slis[y]].lower() not in "abcdefghijklmnopqrstuvwxyz":
+			print("can't put diacritics on symbol character")
+			break
+
+		n=0
+		if remainder>0:
+			n=1
 			remainder-=1
-		else:
-			n = (perletter)*9
+
+		n += quot
 		try:
 			fmt[y] = inp[slis[y]]+cph[:n]
 		except:
-			fmt[y] = inp[slis[y]]+cph[:9]
+			fmt[y] = inp[slis[y]]+cph
 		
 		cph=cph[n:]
 
